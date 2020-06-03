9bb8e3611f23e4b0d90c8acfd891cb9032f91900
itsmath <matheuscardhosoo@gmail.com>
2020-06-02 23:58:27 -0300

    Create list items and create points routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,54 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  response.json({ message: "Hello World" });
+routes.get("/items", async (request, response) => {
+  const items = await knex("items").select("*");
+  const serializedItems = items.map((item) => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+  return response.json(serializedItems);
+});
+
+routes.post("/points", async (request, response) => {
+  const trx = await knex.transaction();
+
+  const image = "fake";
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+  const insertedIds = await trx("points").insert({
+    name,
+    image,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const pointItems = items.map((itemId: number) => {
+    return {
+      point_id: insertedIds[0],
+      item_id: itemId,
+    };
+  });
+  await trx("points_items").insert(pointItems);
+
+  return response.json({ success: true });
 });
 
 export default routes;
