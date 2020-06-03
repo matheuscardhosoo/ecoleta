0d5ee80e9974d529ad19a1265c1a8b1238468c2a
itsmath <matheuscardhosoo@gmail.com>
2020-06-02 22:54:17 -0300

    Clean code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,44 +1,8 @@
-import express, { response } from "express";
+import express from "express";
+
+import routes from "./routes";
 
 const app = express();
-app.use(express.json())
 
-const users = [
-  {
-    name: "Flavia",
-    email: "flavia@email.com",
-  },
-  {
-    name: "Matheus",
-    email: "matheus@email.com",
-  },
-];
-
-app.get("/users", (request, response) => {
-  const search = String(request.query.search);
-  const filteredUsers = search
-    ? users.filter((user) => user.name.includes(search))
-    : users;
-  response.json(filteredUsers);
-});
-
-app.post("/users", (request, response) => {
-  const data = request.body;
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return response.json(user);
-});
-
-app.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-  response.json({
-    id: id,
-    name: "Matheus",
-    email: "matheus@email.com"
-  });
-});
-
+app.use(express.json());
 app.listen(3333);
