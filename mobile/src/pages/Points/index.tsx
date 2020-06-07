30f2a953a9c77f97e4e2b7892e3932f96f8342cb
itsmath <matheuscardhosoo@gmail.com>
2020-06-07 11:34:46 -0300

    Install expo-constants

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-// import { Constants } from "expo";
+import Constants from "expo";
 
 const Points = () => {
   return <View></View>;
@@ -12,8 +12,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 32,
-    // paddingTop: 20 + Constants.statusBarHeight,
-    paddingTop: 20,
+    paddingTop: 20 + Constants.statusBarHeight,
   },
 
   title: {
